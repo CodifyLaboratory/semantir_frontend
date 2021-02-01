f1bc70498628eb056cb194d86409a9f9425895e8
Sayakbai Nazarbekov <ff4rrell@gmail.com>
2021-02-01 18:56:50 +0600

    add: first commit to ff4rrel branch

---
+++ src/pages/mainPage/firstMain.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './mainPage.css';
+
+
+
+const MainPage = ()=> {
+
+    
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default MainPage
